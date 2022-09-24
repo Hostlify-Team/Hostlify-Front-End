f3530e98fb4541f806d7c8dc63f746db9d5306f9
Andres Reynoso <u202010196@upc.edu.pe>
2022-09-23 21:47:21 -0500

    Developed History Page

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/history',
+      name: 'HistoryPage',
+      component: () => import('../views/HistoryView.vue')
     }
   ]
 })
