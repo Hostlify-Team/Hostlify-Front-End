040ef4ddf127499820ef605773c4de37a38c58ce
ZarRubio <89089678+ZarRubio@users.noreply.github.com>
2022-09-27 17:51:27 -0500

    feat(map-hotel): adding map hotel component

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/map-hotel',
+      name: 'map-hotel',
+      component: () => import('../hostlify/map-hotel.vue')
     }
   ]
 })
