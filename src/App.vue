1752a0396e1253f4d9861bf0d840352b872d15eb
Josef Romero Florida <fearriper@proton.me>
2022-09-28 19:25:15 -0500

    Refactor

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script>
- import Rooms_Table from "./History/Rooms_Table.vue";
+ import Rooms_Table from "./hostlify/Rooms_Table.vue";
  export default {
    components: {Rooms_Table}
  }
--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ const router = createRouter({
     {
       path: '/rooms',
       name: 'rooms',
-      component: () => import('../History/Rooms_Table.vue')
+      component: () => import('../hostlify/Rooms_Table.vue')
     },
   ]
 })
