ba5bb3510b671d4ba947fa3f6ce895417bb20607
DiegoTalledoS18 <85322967+DiegoTalledoS18@users.noreply.github.com>
2022-10-18 21:42:57 -0500

    feat(deployment): typicode for deployment

--- src/services/room-services.js
+++ src/services/room-services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const http = axios.create({
-    baseURL:"http://localhost:3000/",
+    baseURL:"https://my-json-server.typicode.com/Hostlify-Team/hostlify-data/",
     headers: { "Content-type": "application/json" },
 })
 
